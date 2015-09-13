f1983bb30061d17a6073bec6c3886a5c679a4762
sugardrunk <jannekahkonen@gmail.com>
2015-09-13 17:59:02 +0300

    remove unused files

--- listeners.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo file_get_contents('http://hear.fi:8000/status.xsl');
-
-?>
--- timer.php
+++
@@ -1,7 +0,0 @@
-<?php header('Content-Type: text/html; charset=ISO-8859-15'); ?>
-
-<?php
-
-echo file_get_contents('http://www.hear.fi/looppi/');
-
-?>
