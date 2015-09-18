6985bae289609bf42a1d08a7c1a28ac0d5ae41ca
sugardrunk <jannekahkonen@gmail.com>
2015-09-18 22:01:02 +0300

    fix fetching the tracks from http to https

--- web/current.php
+++ web/current.php
@@ -1,5 +1,9 @@
 <?php
 
-echo file_get_contents('https://www.last.fm/user/ceniq/');
+$string = file_get_contents('https://www.last.fm/user/ceniq/');
+
+$string = preg_replace("/<img[^>]+\>/i", "", $string);
+
+echo $string;
 
 ?>
--- web/library.php
+++ web/library.php
@@ -1,5 +1,9 @@
 <?php
 
-echo file_get_contents('https://www.last.fm/user/ceniq/library');
+$string = file_get_contents('https://www.last.fm/user/ceniq/library');
+
+$string = preg_replace("/<img[^>]+\>/i", "", $string);
+
+echo $string;
 
 ?>
